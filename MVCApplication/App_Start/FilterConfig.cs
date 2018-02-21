aee55b864ca17a6ea10a577ee11339acbd1bf206
Rupinder Singh <rupindersingh54@live.com>
2018-02-21 21:47:11 +0530

    commmit filter config

--- MVCApplication/App_Start/FilterConfig.cs
+++ MVCApplication/App_Start/FilterConfig.cs
@@ -5,6 +5,7 @@ namespace MVCApplication
 {
     public class FilterConfig
     {
+
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
